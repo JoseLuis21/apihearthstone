c9310c8b9d68c88acc2bdbfd7b1aed20f426694b
JoseLuis21 <josephluihs@gmail.com>
2020-10-18 19:11:00 -0300

    actualizaciones para heroku v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  HEARTHSTONE_API: 'https://us.api.blizzard.com/hearthstone/cards',
+  API_CLIENT: "5cabd2b861294b47850c68937112a903",
+  API_SECRET: "O57xue5E61Wl9c10DydpehqV9CP7LQ2I"
 };
