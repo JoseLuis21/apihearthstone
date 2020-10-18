edcabcc77f69885a11ff7a920194db2c2d0f00b0
JoseLuis21 <josephluihs@gmail.com>
2020-10-18 19:27:26 -0300

    actualizaciones para heroku v3

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/app'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/app'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/app/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
